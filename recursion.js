d0ff5b77eb46cb56dca08d67a9ba85910e8d8540
johnenes <johneneojo1@gmail.com>
2023-06-28 20:43:04 -0700

    recursive

---
+++ recursion.js
@@ -0,0 +1,15 @@
+function power(base, exponent = 2){
+    if (exponent == 0){
+	return 1;
+    }else {
+
+	return base * power( base, exponent -1)
+	
+    }
+
+    
+}
+
+console.log(power (2,5));
+
+
