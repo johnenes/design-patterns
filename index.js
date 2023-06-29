931b3bb6f359b8d14d7a07cb41e91f5823328f30
johnenes <johneneojo1@gmail.com>
2023-06-28 18:39:12 -0700

    conditonal Execution

---
+++ index.js
@@ -0,0 +1,14 @@
+let theNumber = Number(prompt("pick a number"));
+if(!number.isNaN(theNumber){
+
+    console.log("The number is a Square of " + theNumber * theNumber);
+
+
+}else{
+    console.log(" Hey, why not pick a number");
+
+
+    
+	
+   
+   
