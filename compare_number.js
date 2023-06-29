244e7a7fb0a247df1c9f0269e076addcf368c0a7
johnenes <johneneojo1@gmail.com>
2023-06-28 19:29:21 -0700

    Comparing using if else Statement

---
+++ compare_number.js
@@ -0,0 +1,8 @@
+let number = Number(prompt('pick a number');
+if(number < 10 ){
+console.log('Small Number:');
+}else if(number < 100){
+console.log('Medium Number');
+}else{
+console.log('Large Number');
+}
